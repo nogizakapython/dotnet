047075ddd6dae59eb55f84419c218b1f78a97de1
Takao Hattori <nogizakapython@outlook.jp>
2024-07-04 09:34:19 +0900

    Modefy20240704

---
+++ paiza_skill1.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        string line = Console.ReadLine();
+        int num = Int32.Parse(line);
+        Console.WriteLine(num/2);
+    }
+}
