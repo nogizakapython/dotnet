478156a1f1af0d26908fb783b0f042ea49862511
Takao Hattori <nogizakapython@outlook.jp>
2024-07-14 21:09:00 +0900

    Modefy20240714

---
+++ test1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int num1,num2;
+
+    cin >> num1;
+    cin >> num2;
+    cout << num1-num2 << endl;
+    return 0;
+}
