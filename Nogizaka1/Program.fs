fb4c096d59bbdb4f95c18f56d13410d0972a9f95
Takao Hattori <nogizakapython@outlook.jp>
2024-07-17 13:39:47 +0900

    Modefy20240717

---
+++ Nogizaka1/Program.fs
@@ -0,0 +1,12 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let number = 1
+let msg = "秋元真夏"
+if number = 1 then
+  System.Console.WriteLine("test1")
+
+if msg = "秋元真夏" then
+  System.Console.WriteLine("秋元真夏はかわいい")
+
+
+printfn "真夏さんはいつもかわいい"
