90f43c4b1f6a0023b70cc2afc43dd4308051cb10
Takao Hattori <nogizakapython@outlook.jp>
2024-07-25 21:07:34 +0900

    Modefy20240725

---
+++ paiza_d296.fsx
@@ -0,0 +1,8 @@
+let str1 = System.Console.ReadLine()
+let str2 = System.Console.ReadLine()
+let num1 = str1.Length
+let num2 = str2.Length
+if num1 = num2 then
+  System.Console.WriteLine("Yes")
+else
+  System.Console.WriteLine("No")
