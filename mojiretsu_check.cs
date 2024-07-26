a8332084faacbc6b006d37c3c9e41f018ece07a2
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-07-26 11:57:20 +0900

    Modefy20240726

--- mojiretsu_check.cs
+++ mojiretsu_check.cs
@@ -1,3 +1,4 @@
+// ライブラリを読み込む
 using System;
 using System.IO;
 using System.Collections.Generic;
@@ -13,9 +14,13 @@ namespace Mojiretsucheck
         {
             Console.WriteLine("文章を入力してください");
             string sentence = Console.ReadLine();
+            // 改行コードを取り除く
             string sentence1 = sentence.Replace("Environment.NewLine","");
+            // 文字数を変数に取得する。
             int num = sentence1.Length;
+            // 結果をテキストファイルに出力する。
             File.WriteAllText(@"mojiretsu.txt","貴方が入力した文章は" + num.ToString()+ "文字です。");
+            // メモ帳で開く
             System.Diagnostics.Process.Start(@"mojiretsu.txt");
         }
     }
