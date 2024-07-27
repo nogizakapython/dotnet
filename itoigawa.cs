c766b4df76430eeffce4e94543fdbcddd0d9c0bc
Takao Hattori <nogizakapython@outlook.jp>
2024-07-27 17:02:13 +0900

    Modefy20240727

---
+++ itoigawa.cs
@@ -0,0 +1,14 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var line1 = Console.ReadLine();
+        if(line1 == "糸魚川市"){
+            Console.WriteLine("糸魚川市にはデンカ青海工場があります");
+        }
+
+    }
+}
