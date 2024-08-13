ef2fc49bc2693fb596a4140aca4728059895e809
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-08-13 13:32:12 +0900

    Modefy20240813

---
+++ paiza_d300.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int num1,num2;
+    
+    cin >> num1;
+    cin >> num2;;
+    cout << num2 - num1 << endl;
+    return 0;
+}
