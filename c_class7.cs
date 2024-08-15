230026f2d2c791dad133bb3871bd64d4c0de1b84
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-08-15 15:29:05 +0900

    Modefy20240815

---
+++ c_class7.cs
@@ -0,0 +1,34 @@
+using System;
+
+public class User {
+    private string name = "me!";
+    public string Name { 
+        get {return this.name;}
+        set { 
+            
+            if(value != ""){
+                this.name = value;
+            } 
+        }
+    }    
+    
+    
+    public void ShowName(){
+        Console.WriteLine("私は{0}です",this.name);
+    }
+    
+}
+
+class MyApp {
+    static void Main(string[] args){
+        User user = new User();
+        user.ShowName();
+        User user1 = new User();
+        user1.Name = "taguchi";
+        user1.ShowName();
+        user1.Name = "";
+        user1.ShowName();
+        
+
+    }
+}
