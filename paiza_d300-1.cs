c7a90ca292779f4db1a6b4850efb2f45e28953e6
Takao Hattori <nogizakapython@outlook.jp>
2024-08-16 05:35:40 +0900

    Modefy20240816

---
+++ paiza_d300-1.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int line1 = int.Parse(Console.ReadLine());
+        int line2 = int.Parse(Console.ReadLine());
+        Console.WriteLine(line2 - line1);
+    }
+}
