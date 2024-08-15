faf7bb71e587c1f212b5cdcecdc239a7dad3f343
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-08-15 14:04:38 +0900

    Modefy20240815

---
+++ Nogizaka4.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Member {
+    public string name;
+    
+    public Member(string name){
+        this.name = name;
+        // Console.WriteLine("Hi,{0}",this.name);
+    }
+
+    public Member() : this("梅澤美波"){
+    
+    }
+
+    public void SayName(){
+        Console.WriteLine("私は{0}です。",this.name);
+    }
+}
+
+class MyApp {
+    static void Main(string[] args){
+        Member m1 = new Member();
+        m1.SayName();
+        Member m2 = new Member("遠藤さくら");
+        m2.SayName();
+        
+    }
+}
