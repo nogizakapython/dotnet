d062e48da9388cfb3f8d9e9230922ac19653f43d
meitetsu5000 <nogizakapython@outlook.jp>
2024-08-22 15:32:55 +0900

    Modefy20240822

---
+++ TEST2/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+let num1 = System.Console.ReadLine()
+let num2 = System.Console.ReadLine()
+let num3 = int num1
+let num4 = int num2
+let ans = num3 * num4
+System.Console.WriteLine(ans)
+
