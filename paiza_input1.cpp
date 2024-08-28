2cde057bdf7dd0e01944a70e3a0f50db6ae215c0
meitetsu5000 <nogizakapython@outlook.jp>
2024-08-28 12:50:22 +0900

    Modefy20240828

---
+++ paiza_input1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    string str1,str2,str3;
+    cin >> str1 >> str2 >> str3;
+    cout << str1 << endl;
+    cout << str2 << endl;
+    cout << str3 << endl;
+    return 0;
+}
