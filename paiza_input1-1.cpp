0ceef715ef9671252dc8b887718ace2fc0c5c54c
meitetsu5000 <nogizakapython@outlook.jp>
2024-08-28 12:52:27 +0900

    Modefy20240828

---
+++ paiza_input1-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string s[3];
+  cin >> s[0] >> s[1] >> s[2];
+  for(int i = 0; i < 3; i++){
+    cout << s[i] << endl;
+  }
+  return 0;
+}
