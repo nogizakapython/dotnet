b01f793ab2136a8718bc810bab03e85292971e38
meitetsu5000 <nogizakapython@outlook.jp>
2024-09-26 20:20:47 +0900

    Modefy20240926

---
+++ paiza_d307.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int num1,ans;
+    cin >> num1;
+    ans = num1 / 80;
+    cout << ans << endl;
+    return 0;
+}
