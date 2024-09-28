665ee91be32496f63551f96abcceb57d74902b07
meitetsu5000 <nogizakapython@outlook.jp>
2024-09-28 20:34:31 +0900

    Modefy20240928

---
+++ paiza_dtest4.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var array1 = new int[3]{8,1,3};
+
+        foreach(int data in array1){
+            Console.WriteLine(data);
+        }
+    }
+}
