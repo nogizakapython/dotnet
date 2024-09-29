743ff485b086dd5d418192ee8eb2e83479509b8d
meitetsu5000 <nogizakapython@outlook.jp>
2024-09-29 22:53:15 +0900

    Modefy20240929

---
+++ paiza_dtest8-1.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int A = 202;
+        int B = 134;
+        int C = 107;
+        double ans = Math.Pow((A+B) * C,2);
+        Console.WriteLine(ans.ToString());
+    }
+}
