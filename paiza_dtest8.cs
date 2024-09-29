ba15f5213275cdf6b3da28fc68410c1408b484b3
meitetsu5000 <nogizakapython@outlook.jp>
2024-09-29 22:51:06 +0900

    Modefy20240929

---
+++ paiza_dtest8.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int A = 202;
+        int B = 134;
+        int C = 107;
+        int ans1 = (A+B) * C;
+        int ans = ans1 * ans1;
+        Console.WriteLine(ans.ToString());
+    }
+}
