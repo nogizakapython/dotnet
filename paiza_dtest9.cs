0bc504252264ff2e680a728ee4f4c5ae895faef9
meitetsu5000 <nogizakapython@outlook.jp>
2024-09-29 22:56:06 +0900

    Modefy20240929

---
+++ paiza_dtest9.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var N = 0;
+        N += 3286;
+        N *= 4736;
+        N %= 12312;
+        Console.WriteLine(N);
+    }
+}
