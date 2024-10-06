1ff37ebb4ea58677d4188bbaf51ac9a5fd32daf1
meitetsu5000 <nogizakapython@outlook.jp>
2024-10-07 07:32:18 +0900

    Modefy20241007

---
+++ paiza_dtest18.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var line = Console.ReadLine();
+        if(line == "paiza"){
+            Console.WriteLine("YES");
+        } else {
+            Console.WriteLine("NO");
+        }
+    }
+}
