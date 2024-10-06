46fc775575b0be0ca7069122262bbd29034bc9a1
meitetsu5000 <nogizakapython@outlook.jp>
2024-10-07 07:25:37 +0900

    Modefy20241007

---
+++ paiza_dtest16.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        string line = Console.ReadLine();
+        string[] array1 = line.Split(' ');
+        int A,B,C;
+        A = Int32.Parse(array1[0]);
+        B = Int32.Parse(array1[1]);
+        C = Int32.Parse(array1[2]);
+        int N = 0;
+        N += A;
+        N *= B;
+        N %= C;
+        Console.WriteLine(N);
+    }
+}
