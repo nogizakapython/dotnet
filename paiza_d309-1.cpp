a7d1bc1d7bbe2dfb68a79e425e6e69abb54f6b5d
meitetsu5000 <nogizakapython@outlook.jp>
2024-10-11 06:31:01 +0900

    Modefy20241011

---
+++ paiza_d309-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int num1,num2;
+    cin >> num1;
+    cin >> num2;
+    cout << (100-num2) * num1 << endl;
+    return 0;
+}
