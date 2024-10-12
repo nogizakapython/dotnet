14be8ffd5be64179b54808c3bce6217dc3386e22
meitetsu5000 <nogizakapython@outlook.jp>
2024-10-12 20:59:35 +0900

    Modefy20241012

---
+++ Struct1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+// 構造体
+// - 継承ができない
+// - メモリの使い方
+
+struct Point {
+  public int X { get; }
+  public int Y { get; }
+  public Point(int x, int y) {
+    X = x;
+    Y = y;
+  }
+  public void GetInfo() {
+    Console.WriteLine($"({X}:{Y})");
+  }
+}
+
+class MyApp {
+
+  static void Main() {
+    Point p1 = new Point(5, 3);
+    Point p2 = new Point(12, 4);
+    p1.GetInfo();
+    p2.GetInfo();
+  }
+
+}
