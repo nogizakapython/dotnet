f12085854cf6d953b7678f55eed645ced79459ea
meitetsu5000 <nogizakapython@outlook.jp>
2024-10-13 18:10:36 +0900

    Modefy20241013

---
+++ paiza_dtest26-1.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int num = Int32.Parse(Console.ReadLine());
+        //int num = Int32.Parse(num1);
+        for(int i=0;i < num;i++){
+            var line = Console.ReadLine();
+            Console.WriteLine(line);
+        }
+    }
+}
