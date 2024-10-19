7c21dd21ae67903fd423cd409b3e1976ae094855
meitetsu5000 <nogizakapython@outlook.jp>
2024-10-19 09:27:01 +0900

    Modefy20241019

---
+++ paiza_exd4.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        string msg1 = Console.ReadLine();
+        string msg2 = Console.ReadLine();
+        Console.WriteLine(msg1 == msg2 ? "OK" : "NG" );
+    }
+}
