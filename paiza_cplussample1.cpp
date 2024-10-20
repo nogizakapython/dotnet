4c6c3f5fba8e18ebc3b6d8d64874a5997d58d41e
meitetsu5000 <nogizakapython@outlook.jp>
2024-10-21 06:26:08 +0900

    Modefy20241021

---
+++ paiza_cplussample1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    string str1,str2;
+    getline(cin, str1);
+    getline(cin, str2);
+    if(str1 == str2){
+        cout << "OK" << endl;
+    } else {
+        cout << "NG" << endl;
+    }
+    return 0;
+}
