593f936b7f97392542d5bdbb47987bfd7e8b0f46
meitetsu5000 <nogizakapython@outlook.jp>
2024-10-27 18:23:34 +0900

    Modefy2041028

---
+++ uu_prog1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main(void){
+
+    string str1,ans1;
+    cin >> str1;
+    ans1 = "uu_progはキチガイ";
+    if(str1 == ans1){
+        cout << "正解！" + ans1 << endl;
+    }
+
+}
