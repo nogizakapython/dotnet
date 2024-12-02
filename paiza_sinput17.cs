9c4f1ef72536fbd0563cfa825a19535b6d00e1a4
meitetsu5000 <nogizakapython@outlook.jp>
2024-12-02 12:43:01 +0900

    Modefy20241202

---
+++ paiza_sinput17.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int num = 1000;
+        for(int i=0;i<num;i++){
+            var data = Console.ReadLine();
+            Console.WriteLine(data);
+        }
+
+    }
+}
