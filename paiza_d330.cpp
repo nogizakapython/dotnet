55865e9a771344950d474dc6d22a86747fbe75fa
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-12-09 17:10:12 +0900

    Modedfy20241209

---
+++ paiza_d330.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int num1,num2;
+    for(int i=0;i<2;i++){
+        cin >> num1;
+        cin >> num2;
+    }
+    
+    cout << num1 * num2 << endl;
+        
+    return 0;
+}
