d51ea0d9add55842bb98e30bddcf3f30530645a2
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-12-09 17:22:15 +0900

    Modefy20241209

---
+++ paiza_sinput19.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        string data1 = Console.ReadLine();
+        string[] array1 = data1.Split(' ');
+        foreach(string ans in array1){
+            Console.WriteLine(ans);
+        }    
+    }
+}
