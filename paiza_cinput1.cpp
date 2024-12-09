2758f117ed4f5287d350ada244fed58eb7fe536c
meitetsu5000 <nogizakapython@outlook.jp>
2024-12-10 07:26:01 +0900

    Modefy20241210

---
+++ paiza_cinput1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int num;
+    cin >> num;
+    cout << num << endl;
+    //cin >> num;
+    return 0;
+}
