55cd690a3331fab8eb9a158dd4277748c388bb19
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2024-12-20 10:53:38 +0900

    Modefy20241220

---
+++ paiza_sinput27.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var num = Int32.Parse(Console.ReadLine());
+        for(int i=0;i<num;i++){
+            var data = Console.ReadLine();
+            Console.WriteLine(data);
+        }
+        //Console.WriteLine("XXXXXX");
+    }
+}
