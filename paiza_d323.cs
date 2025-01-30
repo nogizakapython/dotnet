1fa2655bb1f4766be04de52c1a9fc1cce4cdc8a2
meitetsu5000 <nogizakapython@outlook.jp>
2025-01-30 13:22:07 +0900

    Modefy20250130

---
+++ paiza_d323.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int n = Int32.Parse(Console.ReadLine());
+        int ans = n * 3;
+        Console.WriteLine(ans);
+    }
+}
