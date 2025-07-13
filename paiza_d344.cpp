99b210a6479e064a148d559e978e464cdf44dff1
meitetsu5000 <nogizakapython@outlook.jp>
2025-07-14 07:20:54 +0900

    Modefy20250714

---
+++ paiza_d344.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int a,b;
+    cin >> a;
+    cin >> b;
+    cout << a * b << endl;
+    return 0;
+}
