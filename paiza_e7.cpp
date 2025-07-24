d96ba2f565bb66f0be0551e4e1856e66b3a5e480
meitetsu5000 <nogizakapython@outlook.jp>
2025-07-25 08:18:38 +0900

    Modefy20250725

---
+++ paiza_e7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int A =  437326;
+    int B = 9085;
+    int ans1 = A / B;
+    int ans2 = A % B;
+    cout << to_string(ans1) + " " + to_string(ans2) << endl;
+    return 0;
+}
