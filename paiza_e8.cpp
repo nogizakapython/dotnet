97d8feffb91ce28cf400e8298e2ae301888c327d
meitetsu5000 <nogizakapython@outlook.jp>
2025-07-25 10:19:26 +0900

    Modefy20250725

---
+++ paiza_e8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int A = 202,B = 134, C = 107;
+    int ans = ((A+B)*C) * ((A+B)*C);
+    cout << ans << endl;
+    return 0;
+}
