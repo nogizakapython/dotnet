af1f853a809c14ea163b85fc50929798b667cfc5
meitetsu5000 <nogizakapython@outlook.jp>
2025-07-26 17:04:04 +0900

    Modefy20250726

---
+++ paiza_e9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int N = 3286;
+    N *= 4736;
+    N %= 12312;
+    cout << N << endl;
+    return 0;
+}
