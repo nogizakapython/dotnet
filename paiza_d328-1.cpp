f44b83fe041547ef4a9529c2a7e7f85cafb5624b
meitetsu5000 <nogizakapython@outlook.jp>
2025-07-26 17:08:30 +0900

    Modefy20250726

---
+++ paiza_d328-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int num1,num2;
+    cin >> num1;
+    cin >> num2;
+    if (num1 > num2){
+        cout << "OVER" << endl;
+    } else {
+        cout << "OK" << endl;
+    }
+        return 0;
+}
