3271e47ae1b8d962e3c15896d903be2e6d280588
meitetsu5000 <nogizakapython@outlook.jp>
2025-08-17 19:01:37 +0900

    Modefy20250817

---
+++ paiza_dsample1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    string str;
+    getline(cin, str);
+    cout << str << endl;
+    return 0;
+}
