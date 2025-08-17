ac0b51b219f1626016cb6ca8d9ea9753205324ec
meitetsu5000 <nogizakapython@outlook.jp>
2025-08-17 18:30:36 +0900

    Modefy20250817

---
+++ paiza_d355.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int num1,num2;
+    int total;
+    cin >> num1;
+    cin >> num2;
+    total = 813 - num1 + num2;
+    cout << total << endl;
+    return 0;
+}
