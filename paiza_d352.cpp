dc0b1737d7d894e7251a4287451be5006d591bb3
meitetsu5000 <nogizakapython@outlook.jp>
2025-08-25 20:52:35 +0900

    Modefy20250825

---
+++ paiza_d352.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int num1,num2,num3,num4,total;
+    cin >> num1;
+    cin >> num2;
+    cin >> num3;
+    cin >> num4;
+    total = num1 * num3 + num2 * num4;
+    cout << total << endl;
+    return 0;
+}
