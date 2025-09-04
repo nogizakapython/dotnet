b2b0771fc8fb001629479fa3b284673e89fe610f
meitetsu5000 <nogizakapython@outlook.jp>
2025-09-04 12:33:19 +0900

    Modefy20250904

---
+++ paiza_e11.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    for(int i=0;i<2;i++){
+        string str;
+        getline(cin, str);
+        cout << str << endl;
+    }
+    return 0;
+}
