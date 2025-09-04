ad33dc2b9b5febde87008cc86c1366e27f511c79
meitetsu5000 <nogizakapython@outlook.jp>
2025-09-04 12:42:38 +0900

    Modefy20250904

---
+++ paiza_e13.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    string str1,str2;
+    cin >> str1;
+    cin >> str2;
+    cout << str1 << endl;
+    cout << str2 << endl;
+
+    return 0;
+}
