19921648e7f3aabe50c34ce29e6ca98900e53ed9
meitetsu5000 <nogizakapython@outlook.jp>
2025-09-09 07:01:00 +0900

    Modefy20250909

---
+++ paiza_e18.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    string str;
+    getline(cin, str);
+    if(str == "paiza"){
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
