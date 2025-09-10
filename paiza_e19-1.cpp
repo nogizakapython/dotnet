7516ff021c0c24adbc102cd12448117842543aae
meitetsu5000 <nogizakapython@outlook.jp>
2025-09-10 15:55:46 +0900

    Modefy20250910

---
+++ paiza_e19-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    if (n <= 100) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
