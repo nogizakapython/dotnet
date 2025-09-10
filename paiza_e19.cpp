a1893c887d241c6cb2027e6c94ebb659ca824309
meitetsu5000 <nogizakapython@outlook.jp>
2025-09-10 15:54:36 +0900

    Modefy20250910

---
+++ paiza_e19.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int num;
+    cin >> num;
+    if(num <= 100){
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
