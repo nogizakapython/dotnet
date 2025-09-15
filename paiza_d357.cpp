5c55e9d45989f0a9228c3957f5c6d829f647f574
meitetsu5000 <nogizakapython@outlook.jp>
2025-09-15 19:19:55 +0900

    Modefy20250915

---
+++ paiza_d357.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    string str1;
+    int leng1;
+    cin >> str1;
+    leng1 = (int)str1.length();
+    cout << str1[leng1-1] << endl;
+    return 0;
+}
