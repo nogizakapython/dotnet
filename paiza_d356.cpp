686df1844e470885ac5b05c0e2e58b56fd419f16
meitetsu5000 <nogizakapython@outlook.jp>
2025-09-15 19:15:26 +0900

    Modefy20250915

---
+++ paiza_d356.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int num1,ans1;
+    cin >> num1;
+    ans1 = int32_t(num1 / 15) * 200;
+    cout << ans1 << endl;
+    return 0;
+}
