f9f378d9b46e974289c188c0d829c616e492fd5b
meitetsu5000 <nogizakapython@outlook.jp>
2025-09-18 12:59:32 +0900

    Modefy20250918

---
+++ paiza_e22.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        cout << i << endl;
+    }
+    return 0;
+}
