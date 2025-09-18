1a6bfc31bef420d5cce3a4979e08e7d3b8cdb32c
meitetsu5000 <nogizakapython@outlook.jp>
2025-09-18 12:52:40 +0900

    Modefy20250918

---
+++ paiza_e21.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int num;
+    cin >> num;
+    if(num == 7){
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    return 0;
+}
