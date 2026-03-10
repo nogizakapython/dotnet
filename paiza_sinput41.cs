247689281b10df8f02788b2f305997bd67f94949
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-03-10 16:36:40 +0900

    Modefy20260310

--- paiza_sinput41.cs
+++ paiza_sinput41.cs
@@ -6,12 +6,9 @@ class Program
     {
         // 自分の得意な言語で
         // Let's チャレンジ！！
-        int n = Int32.Parse(Console.ReadLine());
-        string[] array1 = new string[n];
-        for(int i=0;i<n;i++){
-            string data = Console.ReadLine();
-            array1[i] = data;
-        }
-        Console.WriteLine(array1[7]);
+        string s = Console.ReadLine();
+        int n = s.Length;
+        
+        Console.WriteLine(s.Substring(n-1));
     }
 }
