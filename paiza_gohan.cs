6f8b4c67b3bddcefdb1fbd5fbdd5a0f38a5df4cf
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-03-13 10:57:25 +0900

    Modefy20260313

---
+++ paiza_gohan.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        string line = Console.ReadLine();
+        string[] array1 = line.Split(" ");
+        int H = Int32.Parse(array1[0]);
+        int M = Int32.Parse(array1[1]);
+        if((H >= 7) && (H < 13)){
+            Console.WriteLine("lunch");
+        } else if ((H >= 13) &&  (H < 19))  {
+            Console.WriteLine("dinner");
+        } else {
+            Console.WriteLine("breakfast");
+        } 
+        
+        // Console.WriteLine("XXXXXX");
+    }
+}
