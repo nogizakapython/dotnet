13db5cc967aa582a673e4bf062462e35fd97c2ae
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-03-16 13:31:35 +0900

    Modefy20260316

--- paiza_sinput48.cs
+++ paiza_sinput48.cs
@@ -7,9 +7,10 @@ class Program
         // 自分の得意な言語で
         // Let's チャレンジ！！
         int N = Int32.Parse(Console.ReadLine());
+        int ans = N;
         for(int i = 0; i < 5; i++)
         {
-            N *= N;
+            N *= ans;
         }
         Console.WriteLine(N);
     }
