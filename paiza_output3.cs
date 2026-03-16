cff1a042c72bbaf0e5c71ff4d0bfcaaf9af9f85d
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-03-16 11:31:52 +0900

    Modefy20260316

---
+++ paiza_output3.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Console.WriteLine(8);
+        Console.WriteLine(1);
+        Console.WriteLine(3);
+    }
+}
