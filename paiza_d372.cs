6bca9d69f32e0bedc7a64efb44fc6cadfbd190aa
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-03-25 13:36:52 +0900

    Modefy20260325

---
+++ paiza_d372.cs
@@ -0,0 +1,13 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        string year = Console.ReadLine();
+        string month = Console.ReadLine();
+        string day = Console.ReadLine();
+        Console.WriteLine(year + "/" + month + "/" + day);
+    }
+}
