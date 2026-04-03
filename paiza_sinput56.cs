366151ffbbfbb9a8321d2d06ecc99b49fb8e3edb
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-04-03 17:47:07 +0900

    Modefy20260403

---
+++ paiza_sinput56.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int n = 1000;
+        for(int i=1;i <= n; i++){
+            Console.WriteLine(i);    
+        }
+        
+    }
+}
