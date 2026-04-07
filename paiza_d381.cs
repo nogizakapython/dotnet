267c73d410c95cbf8b5402ceaaf284c0a8eaaad7
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-04-07 11:34:35 +0900

    Modefy20260408

---
+++ paiza_d381.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var line = Console.ReadLine();
+        if(line == "True"){
+            Console.WriteLine("False");
+        } else if (line == "False")
+        {
+            Console.WriteLine("True");
+        }    
+    }
+}
