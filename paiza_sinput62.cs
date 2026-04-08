f956cd7d3d7e3975791f1b29d14d608df1724472
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-04-08 11:38:05 +0900

    Modefy20260408

---
+++ paiza_sinput62.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var line = "8 1 3";
+        Console.WriteLine(line);
+    }
+}
