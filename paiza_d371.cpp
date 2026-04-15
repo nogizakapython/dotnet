e6c35a85123bbdd474cf17d3c7488b83cbb6f8b1
meitetsu5000 <nogizakapython@outlook.jp>
2026-04-15 18:28:15 +0900

    Modefy20260415

---
+++ paiza_d371.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int n;
+    int a;
+    int b;
+    cin >> n;
+    cin >> a;
+    cin >> b;
+    cout << a + b - n << endl;
+    return 0;
+}
