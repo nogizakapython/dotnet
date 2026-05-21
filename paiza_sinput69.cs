780281dea48a2b45df08d25a93d9bbbb715b3c1c
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-05-21 09:18:51 +0900

    Modefy20260521

--- paiza_sinput69.cs
+++ paiza_sinput69.cs
@@ -6,15 +6,9 @@ class Program
     {
         // 自分の得意な言語で
         // Let's チャレンジ！！
-        string ans = "";
-        for(int i=0;i<10;i++){
-            var line = Console.ReadLine();
-            if(i < 9){
-                ans += line + " ";
-            } else {
-                ans += line;
-            }
-        }
-        Console.WriteLine(ans);
+        int n = Int32.Parse(Console.ReadLine());
+        for(int i=1;i<=n;i++){
+            Console.WriteLine(i);
+        }    
     }
 }
