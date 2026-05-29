2787c11b08834af3187317916e8c2202c787151b
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-05-29 15:00:09 +0900

    Modefy20260529

---
+++ paiza_sinput71.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        string p = Console.ReadLine();
+        string q = Console.ReadLine();
+        string r = Console.ReadLine();
+        string ans = p + "|" + q + "|" + r;
+        Console.WriteLine(ans);
+    }
+}
