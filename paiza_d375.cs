6971504145cd555ada63c95bfcbacd0a48d076e8
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-05-29 14:54:47 +0900

    Modefy20260529

---
+++ paiza_d375.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int x = Int32.Parse(Console.ReadLine());
+        int y = Int32.Parse(Console.ReadLine());
+        Console.WriteLine(x * y / 100);
+    }
+}
