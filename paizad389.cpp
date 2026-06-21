1f9f2aa569d7fb621be03a940370f3611db69abf
meitetsu5000 <nogizakapython@outlook.jp>
2026-06-21 11:22:56 +0900

    Modefy20260621

---
+++ paizad389.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    string a,b,c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cout << a + b + c << endl;
+    return 0;
+}
