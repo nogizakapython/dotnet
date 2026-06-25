932f81217db4faf95dc38e87b0d0bc706ccf646c
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-06-25 11:45:34 +0900

    Modefy20260625

---
+++ paiza_template2.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        double n = double.Parse(Console.ReadLine());
+        Console.WriteLine(n + 0.1);
+    }
+}
