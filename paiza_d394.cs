a6f7320aaef734b3ca4e7c295f3612c2a8793c98
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-07-21 15:38:44 +0900

    Modefy20260721

---
+++ paiza_d394.cs
@@ -0,0 +1,15 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int T = Int32.Parse(Console.ReadLine());
+        if(T >= 25){
+            Console.WriteLine("OK");
+        } else {
+            Console.WriteLine("NG");
+        }    
+    }
+}
