738de3d84c889b1960568026b34d064bbe961467
meitetsu5000 <nogizakapython@outlook.jp>
2026-07-22 12:35:34 +0900

    Modefy20260722

--- paiza_template9.cs
+++ paiza_template9.cs
@@ -7,7 +7,7 @@ class Program
         // 自分の得意な言語で
         // Let's チャレンジ！！
         string d = Console.ReadLine();
-        var array1 = d.Split(" ");
+        var array1 = d.Split(' ');
         foreach(string s in array1){
             Console.WriteLine(s);
         }
