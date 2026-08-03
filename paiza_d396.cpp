d47141c3051d81f993b1c37c77114adad25778b5
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-08-03 13:58:20 +0900

    Modefy20260803

---
+++ paiza_d396.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    string str1,str2,str3;
+    string ans;
+    cin >> str1;
+    cin >> str2;
+    cin >> str3;
+    ans = str1 + "/" + str2 + "-" + str3;
+    cout << ans << endl;
+    return 0;
+}
