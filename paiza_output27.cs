8908f05bd9cd9c3eea1b6fc88d538b65e4eb22b7
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-08-06 11:32:58 +0900

    Modefy20260806

---
+++ paiza_output27.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        string[] array1 = {"0 8","1 3"};
+        foreach(var d in array1){
+            Console.WriteLine(d);    
+        }
+        
+    }
+}
