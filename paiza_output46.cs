f2728a1ea20616e0eeb1ebff5adb769d2c7c652c
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-08-18 11:55:20 +0900

    Modefy20260818

---
+++ paiza_output46.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        string s= Console.ReadLine();
+        string t= Console.ReadLine();
+        Console.WriteLine(s + " + " + t + " = " + s + t);
+    }
+}
