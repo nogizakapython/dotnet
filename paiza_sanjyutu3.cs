23f30f0a3b12e68eeb5388e801997ea8f5399264
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-08-26 13:18:09 +0900

    Modefy20260826

---
+++ paiza_sanjyutu3.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        
+        int a = 437326;
+        int b = 9085;
+        int x = a / b;
+        int y = a % b;
+        
+        Console.WriteLine(x.ToString() + " " + y.ToString());
+    }
+}
