2f908e81dd36ac87e385bcd96cc90f1c3ccdee8e
meitetsu5000 <nogizakapython@outlook.jp>
2026-08-28 18:28:41 +0900

    Modefy20260828

---
+++ paiza_d399.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    int n;
+    int m;
+    int p;
+    cin >> n;
+    cin >> m;
+    cin >> p;
+    cout << n * m * p << endl;
+    return 0;
+}
