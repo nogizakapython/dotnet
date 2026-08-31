b023edbcd647b892456ff8ef7378857e46c54fbd
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-08-31 11:21:19 +0900

    Modefy20260831

---
+++ sanjyutu5.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int a = 202;
+        int b = 134;
+        int c = 107;
+        int ans = ((a + b) * c)* ((a+b) * c);
+        Console.WriteLine(ans);
+    }
+}
