64b76737495ae7b5048c061b246396cb50a38ffb
meitetsu5000 <nogizakapython@outlook.jp>
2026-09-01 11:31:28 +0900

    modefy20260901

---
+++ paiza_jikosyokai.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Console.WriteLine("name:ayaka");
+        Console.WriteLine("language:日本語");
+        Console.WriteLine("hitokoto:お願いします");
+    }
+}
