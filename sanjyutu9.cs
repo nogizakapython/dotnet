a0a7eb221647872c466a88b15874f05b7b9fa9db
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-09-04 10:50:36 +0900

    Modefy20260904

---
+++ sanjyutu9.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int N = 10000;
+        N /= 361;
+        N %= 28;
+        Console.WriteLine(N);
+    }
+}
