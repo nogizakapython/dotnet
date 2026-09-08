739d6ecee7619d8b20f7cded9d71be4f8bdff1c0
meitetsu5000 <nogizakapython@outlook.jp>
2026-09-08 09:43:05 +0900

    Modefy20260908

---
+++ paiza_data4.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        var s = Console.ReadLine();
+        if(s == "paiza"){
+            Console.WriteLine("YES");
+
+        } else {
+            Console.WriteLine("NO");
+        }
+    }
+}
