ac4bef668cfda7f6abc50b39fd755d1a02da6f0d
meitetsu5000 <nogizakapython@outlook.jp>
2026-09-09 13:18:45 +0900

    Modefy20260909

---
+++ paiza_d347-1.cs
@@ -0,0 +1,17 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int num1 = Int32.Parse(Console.ReadLine());
+        int num2 = Int32.Parse(Console.ReadLine());
+        if(num1 >= num2){
+            Console.WriteLine("OK");
+        } else {
+            Console.WriteLine("NG");
+        }
+
+    }
+}
