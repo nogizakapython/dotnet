925d8ff1cf3448924ebf5bbafd8421fe3cc43668
meitetsu5000 <nogizakapython@outlook.jp>
2026-09-13 16:06:17 +0900

    Modefy20260913

---
+++ paiza_d401-1.cs
@@ -0,0 +1,15 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        int n = Int32.Parse(Console.ReadLine());
+        if(n >= 8){
+            Console.WriteLine("Yes");
+        } else {
+            Console.WriteLine("No");
+        }
+    }
+}
