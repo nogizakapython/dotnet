437709a90af304468ba62215c40f27907cf20b39
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-09-18 15:02:19 +0900

    Modefy20260918

---
+++ paiza_go7-1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main(){
+	var greeting string
+	var name string
+	var set1 string
+	greeting = "Hello world"
+	name = "Ayaka"
+	set1 = "Go Lang"
+	fmt.Println(greeting)
+	fmt.Println(name)
+	fmt.Println(set1)
+}
+
+
