5d94dfd6586e981f23015a4f591bba5a52c58692
nogizaka Python <92526281+nogizakapython@users.noreply.github.com>
2026-09-18 15:17:42 +0900

    Modefy20260918

---
+++ paiza_go8-1.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func main(){
+	var greeting string
+	greeting = "Hello paiza"
+	fmt.Println(greeting)
+
+	var number int
+	number = 100
+	fmt.Println(number + 30)
+
+	name := "Ayaka"
+	fmt.Println(name)
+
+	Player_name := "kei"
+	fmt.Println(Player_name)
+	
+}
